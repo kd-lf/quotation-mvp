39691165b8caa73e8deb894ed81e01e34eb99f13
kd-lf <liam.flood@kd.kongsberg.com>
2026-02-17 13:37:58 +0100

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/quotation-mvp/',   // <-- add this
+  base: '/quotation-mvp/',
   plugins: [react()],
 })
