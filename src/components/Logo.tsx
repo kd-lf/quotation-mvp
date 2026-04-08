38ddae552202c7231bd1244ebcf38bd2acde7b1b
kd-lf <liam.flood@kd.kongsberg.com>
2026-04-08 14:00:24 +0200

    Updated logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div style={{ marginBottom: 20 }}>
       <img
-        src="https://media.licdn.com/dms/image/v2/D5616AQGASV7fqDbewg/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1681742630644?e=1772668800&v=beta&t=G-b89nqLk6qiP9TBYM5LlL5WESzhi5l3uldK8ziXkkE"
+        src="https://media.licdn.com/dms/image/v2/D4D3DAQHM96oLeOyKBA/image-scale_191_1128/image-scale_191_1128/0/1683886815601/kongsberg_discovery_cover?e=1776254400&v=beta&t=3wQR3fq3tPIXFkB6hCMQENutDEDjoP7h5mcRyvLz7Rc"
         alt="Company Logo"
         style={{ width: "100%", objectFit: "cover", borderRadius: 8 }}
       />
